3c2a5c0d3acf4c20157a1af43b1212e2d186912c
Language-Changer <121141095+Language-Changer@users.noreply.github.com>
2023-04-05 15:15:50 +0800

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,25 +1,27 @@
 //欧拉筛法。
 //素数存在数组p中。
-#include<iostream>
-#include<cstdio>
+#include<bits/stdc++.h>
 using namespace std;
+#define int long long
 
 int n,cnt=0;
 int p[10000010];
 bool is_prime[10000010];
+int sum=0;
 
-int main()
+signed main()
 {
 	cin >> n;
+	is_prime[1] = 1;
 	for(int i=2;i <= n;i++)
 	{
-		if(is_prime[i])
+		if(is_prime[i] == 0)
 		{
 			p[++cnt] = i;
 		}
 		for(int j=1;j <= cnt && p[j] * i <= n;j++)
 		{
-			is_prime[p[j] * i] = 0;
+			is_prime[p[j] * i] = 1;
 			if(i % p[j] == 0)
 			{
 				break;
