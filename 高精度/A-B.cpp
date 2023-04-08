6a725e3a6314e58958e25e579760047e3ab84f03
Language-Changer <121141095+Language-Changer@users.noreply.github.com>
2023-04-08 14:03:42 +0800

    Create A-B.cpp

---
+++ 高精度/A-B.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+string a,b;
+int c[1005],d[1005],e[1006];
+
+int main()
+{
+	cin >> a >> b;
+	if(a.size() < b.size()) swap(a,b);
+	if(a.size() == b.size() && a < b) swap(a,b);
+	
+	for(int i=0;i < a.size();i++) c[a.size()-i] = a[i] - '0';
+	for(int i=0;i < b.size();i++) d[b.size()-i] = b[i] - '0';
+	
+	for(int i=1;i <= max(a.size(),b.size());i++)
+	{
+		e[i] += c[i] - d[i];
+		if(e[i] < 0)
+		{
+			e[i+1] = -1;
+			e[i] += 10;
+		}
+	}
+	int i;
+	for(i=1006;i > 0;i--) if(e[i]) break;
+	for(int j=i;j >= 1;j--) cout << e[j];
+    return 0;
+}
