f485cc0cab285f1df444034b3355aaaf125a8bfa
Dorion Y. Chou <121141095+Language-Changer@users.noreply.github.com>
2023-10-22 09:09:31 +0800

    随机数(C++)

---
+++ 数/cCreator.cpp
@@ -0,0 +1,21 @@
+#include<time.h>
+#include<stdlib.h>
+#include<fstream>
+#include<iostream>
+#define int long long
+
+signed main()
+{
+    int num,MAX_VALUE;
+    std::cin >> num >> MAX_VALUE;
+    srand(time(0));
+    std::ofstream outfile;
+    outfile.open("sort.in");
+    outfile << num << std::endl;
+    for(int i=1;i <= num;i++)
+    {
+        outfile << rand() % (MAX_VALUE + 1) << ' ';
+    }
+    outfile.close();
+    return 0;
+}
